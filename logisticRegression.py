ca8f9a17939f8e6cdde76b963054117ac0478824
monda <monda0524@gmail.com>
2020-04-03 22:02:00 +0900

    add Logistic Regression

---
+++ logisticRegression.py
@@ -0,0 +1,16 @@
+from sklearn.datasets import load_wine
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+# データの読み込み
+wine = load_wine()
+X = wine['data']
+y = wine['target']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+score = model.score(X_test, y_test)
+
+print('score is', score)
