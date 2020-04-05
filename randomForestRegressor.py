49348694e5694eff4cee476a94206a0ea8fc40d6
monda <monda0524@gmail.com>
2020-04-05 15:11:38 +0900

    add Random Forest Regressor

---
+++ randomForestRegressor.py
@@ -0,0 +1,16 @@
+from sklearn.datasets import load_boston
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+
+# データの読み込み
+boston = load_boston()
+X = boston['data']
+y = boston['target']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+model = RandomForestRegressor(random_state=0)
+model.fit(X_train, y_train)
+
+score = model.score(X_test, y_test)
+
+print('score is', score)
