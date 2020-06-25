36d44af0e43841aba80a0188718b691c309ee44e
monda00 <monda0524@gmail.com>
2020-06-25 19:51:57 +0900

    add metrics

---
+++ metrics/r2.py
@@ -0,0 +1,17 @@
+from sklearn.datasets import load_boston
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+
+# データの読み込み
+boston = load_boston()
+X = boston['data']
+y = boston['target']
+X_train, X_test, y_train, y_true = train_test_split(X, y, test_size=0.3, random_state=0)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+print('R2 is', r2_score(y_true, y_pred))
