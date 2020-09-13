6fa26907f22541c46fd32fa58135d6525d7b13d3
monda00 <monda0524@gmail.com>
2020-09-13 14:39:00 +0900

    update multi input and output model

--- functional-api/multi_input_output_model.py
+++ functional-api/multi_input_output_model.py
@@ -6,34 +6,41 @@ num_tags = 12
 num_words = 10000
 num_departments = 4
 
+# ダミーデータの作成
 title_data = np.random.randint(num_words, size=(1280, 10))
 body_data = np.random.randint(num_words, size=(1280, 100))
 tags_data = np.random.randint(2, size=(1280, num_tags)).astype("float32")
 priority_targets = np.random.random(size=(1280, 1))
 dept_targets = np.random.randint(2, size=(1280, num_departments))
 
+# タイトルの層
 title_input = keras.Input(
     shape=(None,), name="title"
 )
+title_features = layers.Embedding(num_words, 64)(title_input)
+title_features = layers.LSTM(128)(title_features)
+
+# 本文の層
 body_input = keras.Input(shape=(None,), name="body")
+body_features = layers.Embedding(num_words, 64)(body_input)
+body_features = layers.LSTM(32)(body_features)
+
+# タグの層
 tags_input = keras.Input(
     shape=(num_tags,), name="tags"
 )
+tags_features = layers.Dense(36, activation='relu')(tags_input)
 
-title_features = layers.Embedding(num_words, 64)(title_input)
-body_features = layers.Embedding(num_words, 64)(body_input)
+# 層を結合
+x = layers.concatenate([title_features, body_features, tags_features])
 
-title_features = layers.LSTM(128)(title_features)
-body_features = layers.LSTM(32)(body_features)
-
-x = layers.concatenate([title_features, body_features, tags_input])
-
-priority_pred = layers.Dense(1, name="priority")(x)
-department_pred = layers.Dense(num_departments, name="department")(x)
+# 出力層
+priority_output = layers.Dense(1, name="priority")(x)
+department_output = layers.Dense(num_departments, name="department")(x)
 
 model = keras.Model(
     inputs=[title_input, body_input, tags_input],
-    outputs=[priority_pred, department_pred],
+    outputs=[priority_output, department_output],
 )
 
 keras.utils.plot_model(model, 'multi_inputoutput_model.png', show_shapes=True)
@@ -44,7 +51,7 @@ model.compile(
         "priority": keras.losses.BinaryCrossentropy(from_logits=True),
         "department": keras.losses.CategoricalCrossentropy(from_logits=True),
     },
-    loss_weights=[1.0, 0.2],
+    loss_weights=[1.0, 0.5],
 )
 
 model.fit(
