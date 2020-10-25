a716e6c2626381e9c5ff8b4cae1417651fe0c84f
monda00 <monda0524@gmail.com>
2020-10-25 22:07:18 +0900

    update keras-rl2

---
+++ reinforce-learning/cart_pole_deep_learning.py
@@ -0,0 +1,36 @@
+import gym
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation, Flatten
+from tensorflow.keras.optimizers import Adam
+from rl.agents.dqn import DQNAgent
+from rl.policy import BoltzmannQPolicy
+from rl.memory import SequentialMemory
+
+# 環境の生成
+env = gym.make('CartPole-v0')
+nb_actions = env.action_space.n
+
+# モデルの定義
+model = Sequential()
+model.add(Flatten(input_shape=(1,) + env.observation_space.shape))
+model.add(Dense(16))
+model.add(Activation('relu'))
+model.add(Dense(16))
+model.add(Activation('relu'))
+model.add(Dense(16))
+model.add(Activation('relu'))
+model.add(Dense(nb_actions))
+model.add(Activation('linear'))
+
+# エージェントの設定
+memory = SequentialMemory(limit=50000, window_length=1)
+policy = BoltzmannQPolicy()
+dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=10,
+               target_model_update=1e-2, policy=policy)
+dqn.compile(Adam(lr=1e-3), metrics=['mae'])
+
+# 学習
+dqn.fit(env, nb_steps=50000, visualize=True, verbose=2)
+
+# 5エピソードで学習したモデルをテスト
+dqn.test(env, nb_episodes=5, visualize=True)
